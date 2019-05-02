8f174b3af661bd577a8938b4b58deee42c7bf5e0
Ysw <ye2813@foxmail.com>
2019-05-02 13:00:03 +0800

    创建项目

---
+++ src/main/java/com/ysw/ymanage/YmanageApplication.java
@@ -0,0 +1,13 @@
+package com.ysw.ymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmanageApplication.class, args);
+    }
+
+}
