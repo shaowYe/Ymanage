f7951eae9b3ba455918be429d1871f3a67c448f4
shaow.ye <ye2813@foxmail.com>
2019-05-09 17:35:53 +0800

    shiro

---
+++ src/main/java/com/ysw/ymanage/entity/Right.java
@@ -0,0 +1,60 @@
+package com.ysw.ymanage.entity;
+
+import java.util.Date;
+
+public class Right {
+    private String id;
+    private String name;
+    private String desc;
+    private String url;
+    private Date createDate;
+    private String createUser;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+}
---
+++ src/main/java/com/ysw/ymanage/entity/User.java
@@ -0,0 +1,78 @@
+package com.ysw.ymanage.entity;
+
+import java.util.Date;
+
+public class User {
+    private String id;
+    private String name;
+    private String userName;
+    private String passWord;
+    private Date creatDate;
+    private String createUser;
+    private Date modDate;
+    private String modUser;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Date getCreatDate() {
+        return creatDate;
+    }
+
+    public void setCreatDate(Date creatDate) {
+        this.creatDate = creatDate;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getModDate() {
+        return modDate;
+    }
+
+    public void setModDate(Date modDate) {
+        this.modDate = modDate;
+    }
+
+    public String getModUser() {
+        return modUser;
+    }
+
+    public void setModUser(String modUser) {
+        this.modUser = modUser;
+    }
+}
