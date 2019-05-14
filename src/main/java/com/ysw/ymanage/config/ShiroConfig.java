00aad494ae8a7f97b38cfedea0deb053f54cd96d
shaow.ye <ye2813@foxmail.com>
2019-05-14 09:10:30 +0800

    增加shiro 配置

--- src/main/java/com/ysw/ymanage/config/ShiroConfig.java
+++ src/main/java/com/ysw/ymanage/config/ShiroConfig.java
@@ -32,7 +32,7 @@ public class ShiroConfig {
     @Bean
     public ShiroFilterChainDefinition shiroFilterChainDefinition() {
         DefaultShiroFilterChainDefinition chain = new DefaultShiroFilterChainDefinition();
-        // 由于demo1展示统一使用注解做访问控制，所以这里配置所有请求路径都可以匿名访问
+        // 测试使用，所以这里配置所有请求路径都可以匿名访问
         chain.addPathDefinition("/**", "anon"); // all paths are managed via annotations
 
         // 这另一种配置方式。但是还是用上面那种吧，容易理解一点。
